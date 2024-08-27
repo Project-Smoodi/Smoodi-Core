56dcb08123388dee7a007ca8e52b5e25e94e1e0a
Daybreak312 <ty82afg12@gmail.com>
2024-08-27 10:39:34 +0900

    FEAT :: ModuleConstructor Targeting Annotation

---
+++ src/main/java/org/smoodi/core/loader/ModuleConstructorTarget.java
@@ -0,0 +1,14 @@
+package org.smoodi.core.loader;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ModuleConstructorTarget {
+
+    TargetBy target = TargetBy.DEFAULT;
+}
+
---
+++ src/main/java/org/smoodi/core/loader/TargetBy.java
@@ -0,0 +1,10 @@
+package org.smoodi.core.loader;
+
+public enum TargetBy {
+
+    DEFAULT,
+
+    EMPTY_CONSTRUCTOR,
+
+    ANNOTATED
+}
