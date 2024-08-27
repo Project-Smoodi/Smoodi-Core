f7d0b68fd0a0aaa4f9bb55997f858035546a37bd
Daybreak312 <ty82afg12@gmail.com>
2024-08-27 10:38:57 +0900

    MOVE :: loader package move

--- src/main/java/org/smoodi/core/SmoodiStarter.java
+++ src/main/java/org/smoodi/core/SmoodiStarter.java
@@ -4,7 +4,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.smoodi.core.init.loader.ModuleLoader;
+import org.smoodi.core.loader.ModuleLoader;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -16,15 +16,21 @@ public final class SmoodiStarter {
 
     public final Logger logger = LoggerFactory.getLogger(SmoodiStarter.class);
 
+    public static Class<?> mainClass = null;
+
     public static void startSmoodi(Class<?> mainClass) {
         final LocalDateTime started = LocalDateTime.now();
+
+        SmoodiStarter.mainClass = mainClass;
 
         instance.moduleLoader.loadModules(mainClass.getPackage().getName());
 
         final LocalDateTime now = LocalDateTime.now();
-        instance.logger.info("Smoodi started on {} seconds. Started at : {}, Initialize finished at : {}",
+        instance.logger.info(
+                "Smoodi started on {} seconds. Started at : {}, Initialize finished at : {}",
                 (double) (Duration.between(now, started).getNano() / 1_000_000_000),
-                started, now);
+                started, now
+        );
     }
 
     // Singleton
--- src/test/java/org/smoodi/core/StartTest.java
+++ src/test/java/org/smoodi/core/StartTest.java
@@ -1,5 +1,8 @@
 package org.smoodi.core;
 
+import org.smoodi.core.loader.ModuleConstructorTarget;
+
+@ModuleConstructorTarget
 public class StartTest {
 
     public static void main(String[] args) {
