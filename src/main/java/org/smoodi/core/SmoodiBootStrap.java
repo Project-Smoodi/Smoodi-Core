677cc0915bb0215787ccaf6ddbbf69b7f1f4d9c6
Daybreak312 <ty82afg12@gmail.com>
2024-09-02 20:15:20 +0900

    REFACT :: Use SubprojectManager

--- src/main/java/org/smoodi/core/SmoodiBootStrap.java
+++ src/main/java/org/smoodi/core/SmoodiBootStrap.java
@@ -8,7 +8,8 @@ import org.smoodi.core.module.loader.PackageBasedModuleLoader;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-import static org.smoodi.core.SmoodiFramework.*;
+import static org.smoodi.core.SmoodiFramework.finishBootStrap;
+import static org.smoodi.core.SmoodiFramework.getStarter;
 
 @Slf4j
 public final class SmoodiBootStrap {
