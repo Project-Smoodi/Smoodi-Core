390415b07b9947b1e23bdef7b71e6e4ef9b3e1db
Daybreak312 <ty82afg12@gmail.com>
2024-09-05 20:01:38 +0900

    FEAT :: Scan 대상 어노테이션 추가

--- src/main/java/org/smoodi/core/module/Module.java
+++ src/main/java/org/smoodi/core/module/Module.java
@@ -1,13 +1,11 @@
 package org.smoodi.core.module;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface Module {
 
-    public boolean isPrimary() default false;
+    boolean isPrimary() default false;
 }
--- src/main/java/org/smoodi/core/module/loader/DefaultModuleClassScanner.java
+++ src/main/java/org/smoodi/core/module/loader/DefaultModuleClassScanner.java
@@ -2,20 +2,29 @@ package org.smoodi.core.module.loader;
 
 import lombok.extern.slf4j.Slf4j;
 import org.reflections.Reflections;
+import org.smoodi.core.module.Configuration;
 import org.smoodi.core.module.Module;
+import org.smoodi.core.module.SmoodiDefaultConfiguration;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.reflections.scanners.Scanners.SubTypes;
+import static org.reflections.scanners.Scanners.TypesAnnotated;
+
 @Slf4j
 public class DefaultModuleClassScanner implements ModuleClassScanner {
+
+    private final org.reflections.util.QueryFunction<org.reflections.Store, Class<?>> reflectionsQuery =
+            SubTypes.of(TypesAnnotated.with(
+                    Module.class, Configuration.class, SmoodiDefaultConfiguration.class
+            )).asClass(ClassLoader.getSystemClassLoader());
 
     @Override
     public Set<Class<?>> getModuleClasses(String basePackage) {
 
-        final Reflections reflections = new Reflections(basePackage);
-
-        final Set<Class<?>> moduleClasses = reflections.getTypesAnnotatedWith(Module.class)
+        final Set<Class<?>> moduleClasses = new Reflections(basePackage)
+                .get(reflectionsQuery)
                 .stream().filter(it -> !it.isAnnotation())
                 .collect(Collectors.toSet());
 
