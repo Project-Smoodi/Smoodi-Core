967f6ea19a0d2d44c7f9382eac96722d34477f28
Daybreak312 <ty82afg12@gmail.com>
2024-09-05 18:57:34 +0900

    FIX :: Module Scan 대상에서 어노테이션 제외

--- src/main/java/org/smoodi/core/module/loader/DefaultModuleClassScanner.java
+++ src/main/java/org/smoodi/core/module/loader/DefaultModuleClassScanner.java
@@ -5,6 +5,7 @@ import org.reflections.Reflections;
 import org.smoodi.core.module.Module;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class DefaultModuleClassScanner implements ModuleClassScanner {
@@ -14,7 +15,9 @@ public class DefaultModuleClassScanner implements ModuleClassScanner {
 
         final Reflections reflections = new Reflections(basePackage);
 
-        final Set<Class<?>> moduleClasses = reflections.getTypesAnnotatedWith(Module.class);
+        final Set<Class<?>> moduleClasses = reflections.getTypesAnnotatedWith(Module.class)
+                .stream().filter(it -> !it.isAnnotation())
+                .collect(Collectors.toSet());
 
         log.info("Scan {} modules with basePackage: \"{}\"", moduleClasses.size(), basePackage);
 
