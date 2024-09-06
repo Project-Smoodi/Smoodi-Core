de7512bbaae043e293e5449df5d95761cf35c010
Daybreak312 <ty82afg12@gmail.com>
2024-09-06 15:02:50 +0900

    FEAT :: Module 등록을 위한 어노테이션 추가 기능

--- src/main/java/org/smoodi/core/module/loader/DefaultModuleClassScanner.java
+++ src/main/java/org/smoodi/core/module/loader/DefaultModuleClassScanner.java
@@ -6,6 +6,11 @@ import org.smoodi.core.module.Configuration;
 import org.smoodi.core.module.Module;
 import org.smoodi.core.module.SmoodiDefaultConfiguration;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -15,14 +20,27 @@ import static org.reflections.scanners.Scanners.TypesAnnotated;
 @Slf4j
 public class DefaultModuleClassScanner implements ModuleClassScanner {
 
+    private static final List<AnnotatedElement> annotations = new ArrayList<>(
+            List.of(Module.class, Configuration.class, SmoodiDefaultConfiguration.class));
+
+    public void addModuleAnnotation(Class<? extends Annotation> annotation) {
+        annotations.add(annotation);
+    }
+
+    @SafeVarargs
+    public final void addModuleAnnotation(Class<? extends Annotation>... annotations) {
+        for (Class<? extends Annotation> annotation : annotations) {
+            addModuleAnnotation(annotation);
+        }
+    }
+
     private final org.reflections.util.QueryFunction<org.reflections.Store, Class<?>> reflectionsQuery =
             SubTypes.of(TypesAnnotated.with(
-                    Module.class, Configuration.class, SmoodiDefaultConfiguration.class
+                    new HashSet<>(annotations)
             )).asClass(ClassLoader.getSystemClassLoader());
 
     @Override
     public Set<Class<?>> getModuleClasses(String basePackage) {
-
         final Set<Class<?>> moduleClasses = new Reflections(basePackage)
                 .get(reflectionsQuery)
                 .stream().filter(it -> !it.isAnnotation())
