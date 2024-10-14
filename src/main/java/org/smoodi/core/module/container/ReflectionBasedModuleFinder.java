8979d6630f7bb03b3d7187459f1e2bac019e6ae3
Daybreak312 <ty82afg12@gmail.com>
2024-10-14 15:18:07 +0900

    SNAPSHOT :: 0.1.1-SNAPSHOT

--- src/main/java/org/smoodi/core/module/container/ReflectionBasedModuleFinder.java
+++ src/main/java/org/smoodi/core/module/container/ReflectionBasedModuleFinder.java
@@ -3,17 +3,27 @@ package org.smoodi.core.module.container;
 import org.reflections.Reflections;
 import org.smoodi.core.SmoodiFramework;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public abstract class ReflectionBasedModuleFinder implements ModuleFinder {
 
-    private final List<Reflections> reflections = List.of(
-            new Reflections(SmoodiFramework.getMainClass().getPackage().getName()),
-            new Reflections(SmoodiFramework.SMOODI_PACKAGE_PREFIX)
-    );
+    private final List<Reflections> reflections;
+
+    {
+        if (SmoodiFramework.getMainClass().getPackage().getName().startsWith(
+                SmoodiFramework.SMOODI_PACKAGE_PREFIX)) {
+            reflections = List.of(
+                    new Reflections(SmoodiFramework.SMOODI_PACKAGE_PREFIX)
+            );
+        } else {
+            reflections = List.of(
+                    new Reflections(SmoodiFramework.getMainClass().getPackage().getName()),
+                    new Reflections(SmoodiFramework.SMOODI_PACKAGE_PREFIX)
+            );
+        }
+    }
 
     protected List<Class<?>> collectWithSubTypes(Class<?> klass) {
 
