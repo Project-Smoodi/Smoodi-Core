143e29e8e960e3bf1b38f33b4921f472866fdc22
Daybreak312 <ty82afg12@gmail.com>
2024-10-15 21:25:50 +0900

    ANNO :: 모듈 관련 에러 Docs

--- src/main/java/org/smoodi/core/module/ModuleCreationError.java
+++ src/main/java/org/smoodi/core/module/ModuleCreationError.java
@@ -1,9 +1,10 @@
 package org.smoodi.core.module;
 
 /**
- * Error during module initialization
+ * <p>모듈 객체를 생성하는 과정에서 발생한, 회복 불가능한 에러.</p>
  *
- *
+ * @author Daybreak312
+ * @see ModuleDeclareError
  */
 public class ModuleCreationError extends Error {
 
--- src/main/java/org/smoodi/core/module/ModuleDeclareError.java
+++ src/main/java/org/smoodi/core/module/ModuleDeclareError.java
@@ -1,9 +1,10 @@
 package org.smoodi.core.module;
 
 /**
- * Error during module initialization
+ * <p>모듈의 선언이 잘못되어 사용할 수 없는 경우 발생하는 에러.</p>
  *
- *
+ * @author Daybreak312
+ * @see ModuleCreationError
  */
 public class ModuleDeclareError extends Error {
 
