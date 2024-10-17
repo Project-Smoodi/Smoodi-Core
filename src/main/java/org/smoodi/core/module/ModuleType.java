1c524a257227da061b20ba0f2751ee600e39f700
Daybreak312 <ty82afg12@gmail.com>
2024-10-17 11:17:36 +0900

    REFACT :: HierarchyModuleType과 ModuleType 병합

--- src/main/java/org/smoodi/core/module/ModuleType.java
+++ src/main/java/org/smoodi/core/module/ModuleType.java
@@ -4,8 +4,10 @@ import lombok.Getter;
 import org.smoodi.annotation.NotNull;
 import org.smoodi.annotation.StaticFactoryMethod;
 import org.smoodi.core.module.loader.initializer.DefaultModuleInitConstructorSearcher;
+import org.smoodi.core.util.Nullability;
 
 import java.lang.reflect.Constructor;
+import java.util.Set;
 
 /**
  * <p>{@link org.smoodi.core.annotation.Module 모듈} 초기화에서 요구되는 {@link Class 클래스 정보}와 기타 추가적인 정보를 묶은 클래스.</p>
@@ -20,7 +22,7 @@ public class ModuleType<T> {
     protected final Class<T> klass;
 
     @Getter
-    protected final boolean isSingleton;
+    protected final boolean isCreatable;
 
     protected Constructor<T> moduleInitConstructor;
 
@@ -32,45 +34,67 @@ public class ModuleType<T> {
         return this.moduleInitConstructor;
     }
 
+    private final Set<ModuleType<? extends T>> lowerTypes;
+
+    @Getter
+    private int initializedLowerModuleTypes = 0;
+
     protected ModuleType(
             Class<T> klass,
-            boolean isSingleton
+            Set<ModuleType<? extends T>> lowerTypes
     ) {
+        assert klass != null;
+        assert lowerTypes != null;
+
         this.klass = klass;
-        this.isSingleton = isSingleton;
+        this.isCreatable = isCreatableKlass(klass);
+        this.lowerTypes = lowerTypes;
     }
 
     protected ModuleType(
             Class<T> klass,
-            boolean isSingleton,
-            Constructor<T> moduleInitConstructor
+            Constructor<T> moduleInitConstructor,
+            Set<ModuleType<? extends T>> lowerTypes
     ) {
+        assert klass != null;
+        assert moduleInitConstructor != null;
+        assert lowerTypes != null;
+
         this.klass = klass;
-        this.isSingleton = isSingleton;
+        this.isCreatable = isCreatableKlass(klass);
         this.moduleInitConstructor = moduleInitConstructor;
+        this.lowerTypes = lowerTypes;
+    }
+
+    private boolean isCreatableKlass(Class<T> klass) {
+        return !klass.isAnnotation() && !klass.isEnum() && !klass.isInterface() && !klass.isPrimitive() && !klass.isAnonymousClass();
     }
 
     @StaticFactoryMethod
     @NotNull
-    public static <T> ModuleType<T> of(Class<T> klass) {
-        return new ModuleType<>(klass, true);
+    public static <T> ModuleType<T> of(Class<T> klass, Set<ModuleType<? extends T>> lowerTypes) {
+        return Nullability.firstOrSecondIfNull(
+                ModuleTypeContainer.getModuleType(klass),
+                new ModuleType<>(klass, lowerTypes)
+        );
     }
 
     @StaticFactoryMethod
     @NotNull
-    public static <T> ModuleType<T> of(Class<T> klass, boolean isSingleton) {
-        return new ModuleType<>(klass, isSingleton);
+    public static <T> ModuleType<T> of(Constructor<T> moduleInitConstructor, Set<ModuleType<? extends T>> lowerTypes) {
+        return Nullability.firstOrSecondIfNull(
+                ModuleTypeContainer.getModuleType(moduleInitConstructor.getDeclaringClass()),
+                new ModuleType<>(
+                        moduleInitConstructor.getDeclaringClass(),
+                        moduleInitConstructor,
+                        lowerTypes
+                )
+        );
     }
 
-    @StaticFactoryMethod
-    @NotNull
-    public static <T> ModuleType<T> of(Constructor<T> moduleInitConstructor) {
-        return new ModuleType<>(moduleInitConstructor.getDeclaringClass(), true, moduleInitConstructor);
-    }
-
-    @StaticFactoryMethod
-    @NotNull
-    public static <T> ModuleType<T> of(Constructor<T> moduleInitConstructor, boolean isSingleton) {
-        return new ModuleType<>(moduleInitConstructor.getDeclaringClass(), isSingleton, moduleInitConstructor);
+    public void markInit(ModuleType<? extends T> type) {
+        if (this.lowerTypes.contains(type)) {
+            this.initializedLowerModuleTypes++;
+        }
     }
 }
