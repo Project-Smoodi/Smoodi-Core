ae405704e633451677f657010115e705d17ba35a
Daybreak312 <ty82afg12@gmail.com>
2024-10-18 15:37:42 +0900

    FEAT :: 사용자 모듈이 없을 경우 경고 표시

--- src/main/java/org/smoodi/core/module/loader/ModuleLoaderComposite.java
+++ src/main/java/org/smoodi/core/module/loader/ModuleLoaderComposite.java
@@ -30,9 +30,14 @@ public class ModuleLoaderComposite implements ModuleLoader {
 
         totalLoadedModules += staticModuleLoader.loadModules();
         totalLoadedModules += smoodiProjectModuleLoader.loadModules();
-        totalLoadedModules += packageBasedModuleLoader.loadModules();
+        var userDefinedModules = packageBasedModuleLoader.loadModules();
+        totalLoadedModules += userDefinedModules;
 
         final int moduleContainerModules = SmoodiFramework.getInstance().getModuleContainer().getModuleCount();
+
+        if (userDefinedModules == 0) {
+            log.warn("The number of modules other than smoodi driven module is \"0\". The classes in your project may be \"Module-Private\". If so, smoodi cannot scan your modules.");
+        }
 
         if (log.isDebugEnabled() && totalLoadedModules != moduleContainerModules) {
             log.warn("Smoodi driven loaders loaded {} modules BUT ModuleContainer containing {} modules.", totalLoadedModules, moduleContainerModules);
