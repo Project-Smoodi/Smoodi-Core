5c8f85c7b88592cd580afba260d53b2f88ac520b
Daybreak312 <ty82afg12@gmail.com>
2024-10-24 10:15:04 +0900

    TEST :: 중복 기본 모듈 존재 상황 테스트 추가

---
+++ src/test/java/com/smoodi/module/DuplicatePrimaryModuleDefineTests.java
@@ -0,0 +1,33 @@
+package com.smoodi.module;
+
+import com.smoodi.module.duplicateprimarymodule.DuplicatePrimaryModuleDefine;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.smoodi.core.TestBase;
+import org.smoodi.core.module.ModuleDeclareError;
+import org.smoodi.core.module.loader.DefaultModuleClassScanner;
+import org.smoodi.core.module.loader.DefaultModuleInitializer;
+import org.smoodi.core.module.loader.MainClassPackageBasedModuleLoader;
+import org.smoodi.core.module.loader.ModuleLoader;
+
+@SuppressWarnings("NonAsciiCharacters")
+@Tag("forked")
+public class DuplicatePrimaryModuleDefineTests {
+
+    private ModuleLoader createModuleLoader() {
+        return new MainClassPackageBasedModuleLoader(
+                new DefaultModuleClassScanner(),
+                new DefaultModuleInitializer()
+        );
+    }
+
+    @Test
+    public void 기본_모듈_다수_정의_상황() {
+        TestBase.initTest();
+        TestBase.setMainClass(DuplicatePrimaryModuleDefine.class);
+        final ModuleLoader moduleLoader = createModuleLoader();
+
+        Assertions.assertThrows(ModuleDeclareError.class, moduleLoader::loadModules);
+    }
+}
