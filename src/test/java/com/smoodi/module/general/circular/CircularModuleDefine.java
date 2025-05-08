7b284ba64ba3b2d7bb05f9fd326d47f45db4cae7
Daybreak312 <ty82afg12@gmail.com>
2025-05-09 08:52:52 +0900

    TEST :: Add testcase - interface-based circular dependency

--- src/test/java/com/smoodi/module/general/circular/CircularModuleDefine.java
+++ src/test/java/com/smoodi/module/general/circular/CircularModuleDefine.java
@@ -27,4 +27,55 @@ public class CircularModuleDefine {
         ) {
         }
     }
+
+    public interface InterfaceA {
+    }
+
+    public interface InterfaceB {
+    }
+
+    public interface InterfaceC {
+    }
+
+    @Module
+    public static class InterfaceAImpl implements InterfaceA {
+        public InterfaceAImpl(
+                InterfaceB interfaceB
+        ) {
+        }
+    }
+
+    @Module
+    public static class InterfaceBImpl implements InterfaceB {
+        public InterfaceBImpl(
+                InterfaceC interfaceC
+        ) {
+        }
+    }
+
+    @Module
+    public static class InterfaceCImpl implements InterfaceC {
+        public InterfaceCImpl(
+                InterfaceA interfaceA
+        ) {
+        }
+    }
+
+    public interface InterfaceD {
+    }
+
+    @Module
+    public static class InterfaceDImpl implements InterfaceD {
+        public InterfaceDImpl(
+                ModuleD moduleD
+        ) {
+        }
+    }
+
+    public static class ModuleD {
+        public ModuleD(
+                InterfaceD interfaceD
+        ) {
+        }
+    }
 }
