4dd91806ef75575f6d4e89c7904290eec0787c06
Daybreak312 <ty82afg12@gmail.com>
2025-05-14 14:40:23 +0900

    SNAPSHOT :: v0.1.5 - bug fixes

--- src/main/java/org/smoodi/core/module/container/PrimaryModuleFinder.java
+++ src/main/java/org/smoodi/core/module/container/PrimaryModuleFinder.java
@@ -2,13 +2,11 @@ package org.smoodi.core.module.container;
 
 import org.smoodi.annotation.NotNull;
 import org.smoodi.annotation.array.EmptyableArray;
-import org.smoodi.core.annotation.Module;
-import org.smoodi.core.module.ModuleDeclareError;
 import org.smoodi.core.module.ModuleType;
-import org.smoodi.core.util.AnnotationUtils;
 import org.smoodi.core.util.ModuleUtils;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.SequencedSet;
 
 class PrimaryModuleFinder implements ModuleFinder {
 
@@ -16,43 +14,14 @@ class PrimaryModuleFinder implements ModuleFinder {
     @NotNull
     @Override
     public <T> SequencedSet<T> find(@NotNull ModuleSet moduleSet, @NotNull ModuleType<T> moduleType) {
+        SequencedSet<T> sequenced = moduleSet.get(ModuleUtils.findPrimaryModuleType(moduleType));
 
-        final Set<T> found = new HashSet<>();
-
-        final List<ModuleType<? extends T>> subTypes = new ArrayList<>(moduleType.getSubTypes());
-        subTypes.add(moduleType);
-
-        subTypes.forEach(subType -> {
-            if (moduleSet.get(subType) != null) {
-                found.addAll(
-                        moduleSet.get(subType)
-                );
-            }
-        });
-
-        if (found.size() == 1) {
-            SequencedSet<T> sequenced = new TreeSet<>(ModuleUtils.comparator());
-            sequenced.addAll(found);
-            return Collections.unmodifiableSequencedSet(sequenced);
-        }
-
-        var primary = found.stream().filter(
-                it -> Objects.requireNonNull(
-                        AnnotationUtils.findIncludeAnnotation(it.getClass(), Module.class)
-                ).isPrimary()
-        ).toList();
-
-        if (primary.size() > 1) {
-            throw new ModuleDeclareError("Many primary module found. Primary module MUST BE one: " + moduleType.getKlass().getName());
-        } else if (primary.isEmpty() && found.size() > 1) {
-            throw new ModuleDeclareError("Many modules found BUT the primary module does not exist: " + moduleType.getKlass().getName());
-        } else if (primary.isEmpty() /* && found.isEmpty() => true */) {
+        if (sequenced.isEmpty()) {
             return Collections.emptySortedSet();
+        } else if (sequenced.size() > 1) {
+            throw new IllegalStateException("More than one primary module found.");
         }
 
-
-        SequencedSet<T> sequenced = new TreeSet<>(ModuleUtils.comparator());
-        sequenced.addAll(found);
         return Collections.unmodifiableSequencedSet(sequenced);
     }
 }
